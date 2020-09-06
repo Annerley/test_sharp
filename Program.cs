60bbce423df74b2f4fc9e4a772873a754e732471
Дарья Лапутина <laputina975@gmail.com>
2020-09-06 23:45:28 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace test_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = Console.ReadLine();
+            NumberFormatInfo numberFormatInfo = new NumberFormatInfo()
+            {
+                NumberDecimalSeparator = ".",
+            };
+
+            double a = Convert.ToDouble(data, numberFormatInfo);
+            Console.WriteLine(a);
+           }
+    }
+}
